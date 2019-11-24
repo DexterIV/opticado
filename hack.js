3effc3408b4287bd095459eaca05b048515553f3
DexterIV <wiedzmenpl@gmail.com>
2019-11-24 23:14:18 +0100

    Add files via upload

---
+++ hack.js
@@ -0,0 +1,5 @@
+var iframe = document.getElementById('overlay');
+var doc = iframe.contentWindow.document;
+
+doc.getElementById('WIX_ADS').style.display='none';
+doc.getElementById('SITE_HEADER').style.margin=0;
